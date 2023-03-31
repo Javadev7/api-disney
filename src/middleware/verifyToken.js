855f550fbae5d4e91ca459a7135203b2fdbf0095
Javadeveloper7 <95065154+Javadeveloper7@users.noreply.github.com>
2023-03-30 23:27:52 -0500

    ruta privada

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
-  const token = req.headers.authorization;
-  if (!token) {
+  const bearer = req.headers.authorization;
+  if (!bearer) {
     return res.status(401).json({ message: 'No se ha proporcionado un token' });
   }
   try {
+    const token = bearer.replace('Bearer ', '')
+    console.log(token);
     const decoded = jwt.verify(token, 'tu_secreto');
     req.user = decoded;
     next();
